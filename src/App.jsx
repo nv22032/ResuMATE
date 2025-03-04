60a2de2732e7deac466ebfef493fe5e04cf15bea
nv22032 <150386492+nv22032@users.noreply.github.com>
2025-03-04 12:19:20 +0000

    Added Header 48:12

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
 import { useState } from 'react'
+import Header from './components/custom/Header'
 
 function App () {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App () {
 
   return (
     <>
+    <Header/>
     <Outlet/>
     </>
   )
