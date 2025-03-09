ccb505ce8d87f0275b6ffc01b5d00c687b6c307d
nv22032 <150386492+nv22032@users.noreply.github.com>
2025-03-09 23:44:16 +0000

    Abdulla Mahfouz turn 1:30:17

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AddResume from './components/AddResume'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='p-10 md:px-20 lg:px-32'>
+      <h2 className='font-bold text-3xl'>My Resumes</h2>
+      <p>Begin creating AI resume for your next job role.</p>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10'>
+        <AddResume />
+      </div>
+    </div>
   )
 }
 
